92b26aed8d8660b68070d49ddf81222a6313c61e
arrizal27 <fajarsidiq3000@gmail.com>
2021-04-22 21:04:16 +0700

    cleaning

--- app/src/main/java/com/smkn4bdg/jelitapicker/request/RiwayatActivity.java
+++ app/src/main/java/com/smkn4bdg/jelitapicker/request/RiwayatActivity.java
@@ -21,7 +21,7 @@ public class RiwayatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_riwayat);
+        setContentView(R.layout.activity_request);
 
         findView();
 
